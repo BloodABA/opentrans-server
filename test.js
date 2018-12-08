8ca79d0bd8b08b78a8f7ebf5c9b628b03d0b914d
schio <ghsehr1@gmail.com>
2018-12-08 21:45:35 +0900

    비번 단방향 암호화 추가

--- test.js
+++ test.js
@@ -1,30 +1,5 @@
 const ABAFunc = require('./func')
 
-<<<<<<< HEAD
+
+console.log(ABAFunc.passwordHash());
 console.log(ABAFunc.passwordResetHash("1234"));
-=======
-// console.log("Email Validate")
-// console.log(ABAFunc.isValidEmail("JTJISGOD"));
-// console.log(ABAFunc.isValidEmail("jtisgod@gmail.com"));
-
-console.log("Is Valid Username")
-console.log(ABAFunc.isValidUsername("jt!jisgod"))
-
-console.log(ABAFunc.isValidUsername("jtjisgod"))
-
-console.log(ABAFunc.isValidUsername("jtji sgod"))
-
-console.log(ABAFunc.isValidUsername("jtjis-god"))
-console.log(ABAFunc.isValidUsername("jtji_sgod"))
-
-console.log(ABAFunc.isValidUsername("jt!jis0god"))
-
-console.log(ABAFunc.isValidUsername("jtjis0god"))
-
-console.log(ABAFunc.isValidUsername("jtji0 sgod"))
-
-console.log(ABAFunc.isValidUsername("jtj0is-god"))
-console.log(ABAFunc.isValidUsername("jt0ji_sgod"))
-
-// console.log(ABAFunc.passwordHash("JTJISGOD"));
->>>>>>> c9b28786947c07e43d0813f6432563fd7586dabb
