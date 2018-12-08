92fa911e823a613a937841d11a44eaa1436e7a38
Jangtaejin <jtjisgod@gmail.com>
2018-12-08 17:28:44 +0900

    Accounts 기본 스켈레톤 코드 작성완료
    Completed

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,26 +1,46 @@
 const DB_Accounts = require('../models/accounts');
 const router = require('express').Router();
 
+login = (req, res) => {
+}
+
+register = (req, res) => {   
+}
+
+find_username = (req, res) => {   
+}
+
+find_password = (req, res) => {   
+}
+
+find_password_hash = (req, res) => {   
+}
+
+profile = (req, res) => {   
+}
+
+project = (req, res) => {   
+}
+
 //# 로그인
 router.get('/login', login(req, res));
 
 //# 회원가입
-router.get('/register', login(req, res));
+router.get('/register', register(req, res));
 
 //# 아이디 찾기
-router.get('/find_username', login(req, res));
+router.get('/find_username', find_username(req, res));
 
 //# 비밀번호 찾기
-router.get('/find_password', login(req, res));
+router.get('/find_password', find_password(req, res));
 
 //# 비밀번호 찾기 + 비밀번호 재설정
-router.get('/find_password/<hash>', login(req, res));
+router.get('/find_password/<hash>', find_password_hash(req, res));
 
 //# 프로필 ( 정보 열람 )
-router.get('/profile/<username>', login(req, res));
+router.get('/profile/<username>', profile(req, res));
 
 //# 프로젝트 리스트
-router.get('/project/list/<username>', login(req, res));
-
+router.get('/project/list/<username>', project(req, res));
 
 module.exports = router;
